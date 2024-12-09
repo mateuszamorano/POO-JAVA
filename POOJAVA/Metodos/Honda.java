a1f9d171e1091d58ca4fb612e27c37e92b2fcb89
Mateus Zamorano <52588123+mateuszamorano@users.noreply.github.com>
2024-12-09 12:05:49 -0300

    Add files via upload

---
+++ POOJAVA/Metodos/Honda.java
@@ -0,0 +1,18 @@
+package Metodos;
+
+public class Honda extends Carro {
+	
+	public String motor;
+		
+		public Honda() {
+	}
+		public Honda(String modelo) {
+			
+		}
+		public Honda(String modelo, String cor) {
+			
+		}
+		public Honda(String modelo, String cor, double preco) {
+		super(modelo, cor, preco);
+		}
+}
